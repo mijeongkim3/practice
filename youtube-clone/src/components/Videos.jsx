e9fdf9bf4267d1bd40c289bc811932bbd9bb4b20
mijeongkim3 <yellowredcandy3@gmail.com>
2023-01-31 15:38:42 +0900

    feat:20230131

--- youtube-clone/src/components/Videos.jsx
+++ youtube-clone/src/components/Videos.jsx
@@ -3,15 +3,14 @@ import { Stack, Box } from '@mui/material';
 import { VideoCard, ChannelCard } from './';
 
 const Videos = ({ videos }) => {
-  // console.log(videos); //프롭스로 비디오를..분해해서..
   return (
     <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {/* {videos.map((item, idx) => (
+      {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {item.id.channelId && <ChannelCard channelDetail={item} />}
         </Box>
-      ))} */}
+      ))}
     </Stack>
   );
 };
